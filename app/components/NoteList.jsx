17df89a5ebd380d0a36165205dab636e7cbabcbe
Shawn Johnson <johnson.sg@gmail.com>
2023-03-16 09:05:45 -0400

    format date

--- app/components/NoteList.jsx
+++ app/components/NoteList.jsx
@@ -11,18 +11,18 @@ function NoteList({ notes }) {
             <article>
               <header>
                 <ul className='note-meta'>
-                  <li>#{index + 1}</li>
                   <li>
                     <time dateTime={note.dateAdded}>
                       {new Date(note.dateAdded).toLocaleDateString('en-US', {
                         day: 'numeric',
                         month: 'short',
-                        year: 'numeric',
-                        hour: '2-digit',
-                        minute: '2-digit'
+                        year: 'numeric'
+                        // hour: '2-digit',
+                        // minute: '2-digit'
                       })}
                     </time>
                   </li>
+                  <li>#{index + 1}</li>
                 </ul>
                 <h2>{note.title}</h2>
               </header>
